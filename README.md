cec8fa8afa08fc2c6fc5863534d712029e3c8ea2
Michael Hartisch <michaelhartisch@gmx.de>
2025-06-06 16:30:11 +0200

    Improved automatic detection of GUROBI_HOME

--- README.md
+++ README.md
@@ -10,5 +10,10 @@ EQuIPS is an expansion-based solver for Quantified Integer Programs
 # File Format
 Instances must be in the QLP file format [(see this documentation)](https://yasolqipsolver.github.io/yasol.github.io/About_Yasol/#the-qlp-file-format). 
 
+# Troubleshooting
+If you have trouble compiling or running EQuIPS, here is a list of potential fixes
+* To help cmake find gurobi, set environment variable GUROBI_HOME to your Gurobi directory. This directory should contain the *dir* folder. If you do not explicitly provide this path, cmake will search for it, but it is more reliable to explicitly provide it, either by setting the environment variable variabe GUROBI_HOME, or use the cmake option -DGUROBI_HOME=/path/to/gurobi.
+* To run EQuIPS, you also need a valid Gurobi license. You must set the environment variable variabe GRB_LICENSE_FILE to this file. Otherwise, you will simply get an (unintelligible) message "libc++abi: terminating due to uncaught exception of type GRBException", when trying to run EQuIPS
+
 # References
-Paper "An Expansion-Based Approach for Quantified Integer Programming" to appear in the proceeding of CP2025.
+Paper "An Expansion-Based Approach for Quantified Integer Programming" to appear in the proceeding of CP2025 [preprint]](https://arxiv.org/abs/2506.04452)
--- cmake/modules/FindGUROBI.cmake
+++ cmake/modules/FindGUROBI.cmake
@@ -1,30 +1,77 @@
 # FindGUROBI.cmake
+if (NOT DEFINED GUROBI_HOME)
+    if (DEFINED ENV{GUROBI_HOME})
+        set(GUROBI_HOME "$ENV{GUROBI_HOME}" CACHE PATH "Gurobi root directory")
+    endif()
+endif()
 
-# Locate Gurobi include directory
+string(ASCII 27 ESC)
+set(ORANGE "${ESC}[38;5;208m")
+set(BLUE "${ESC}[1;34m")
+set(NC "${ESC}[0m") # Reset/No Color
+
+if (NOT GUROBI_HOME)
+    message("${BLUE} No path to the root directory of gurobi was provided. I now try to find it myself! ${NC}")
+    file(GLOB_RECURSE SEARCH_DIRS_A "/Library/gurobi*/**/gurobi_c++.h")
+    file(GLOB_RECURSE SEARCH_DIRS_B "/opt/gurobi*/**/gurobi_c++.h")
+    file(GLOB_RECURSE SEARCH_DIRS_C "${HOME}/gurobi/gurobi*/**/gurobi_c++.h")
+
+    list(APPEND GUROBI_DIRS ${SEARCH_DIRS_A})
+    list(APPEND GUROBI_DIRS ${SEARCH_DIRS_B})
+    list(APPEND GUROBI_DIRS ${SEARCH_DIRS_C})
+    if(GUROBI_DIRS)
+        message(STATUS "I found these headers:")
+        foreach(GUROBI_DIR ${GUROBI_DIRS})
+            message(STATUS "${GUROBI_DIR}")
+        endforeach()
+        foreach(GUROBI_DIR ${GUROBI_DIRS})
+            set(GUROBI_HOME "${GUROBI_DIR}") #/Library/gurobi1200/macos_universal2/lib/gurobi_c++.h
+            get_filename_component(GUROBI_HOME "${GUROBI_HOME}" DIRECTORY)  #/Library/gurobi1200/macos_universal2/lib
+            get_filename_component(GUROBI_HOME "${GUROBI_HOME}" DIRECTORY)  #/Library/gurobi1200/macos_universal2
+
+            if(NOT EXISTS "${GUROBI_HOME}/lib")
+                continue()
+            else()
+                message("${BLUE}This folder was selected: ${GUROBI_HOME}")
+                message("${BLUE}If you wish to select another Gurobi version, specify it via -D option, or by setting the environment variable, e.g. 
+    ${ORANGE} cmake -B build -DGUROBI_HOME=/opt/gurobi1200/linux64 
+${BLUE} or 
+    ${ORANGE} export GUROBI_HOME=/Library/gurobi1200/macos_universal2 ")
+                break()
+            endif()
+        endforeach()
+    endif()
+    if (NOT GUROBI_HOME)
+        message(FATAL_ERROR "I was not able to find the root directory of Gurobi. Provide the respective path, which might look like this: -DGUROBI_HOME=/opt/gurobi1200/linux64")
+    else()
+        #message(STATUS "Found root. GUROBI_HOME: ${GUROBI_HOME}")
+    endif()
+else()
+    if(NOT EXISTS "${GUROBI_HOME}/lib")
+        message(FATAL_ERROR "The provided GUROBI_HOME does not seem to contain the folder 'lib'. Maybe a typo?")
+    endif()
+endif()
+
+ # Locate Gurobi include directory
 find_path(GUROBI_INCLUDE_DIRS
-    NAMES     gurobi_c++.h
-    HINTS     ${GUROBI_DIR} $ENV{GUROBI_HOME}
-    PATH_SUFFIXES include
+NAMES     gurobi_c++.h
+HINTS     ${GUROBI_HOME}  ${GUROBI_HOME}   
+
+PATH_SUFFIXES include
 )
 
 # Locate dynamic core library (e.g. libgurobi100.so)
 find_library(GUROBI_LIBRARY
     NAMES     gurobi    gurobi120    gurobi110    gurobi100
-    HINTS     ${GUROBI_DIR} $ENV{GUROBI_HOME}
-    PATH_SUFFIXES lib
+    PATHS     "${GUROBI_HOME}/lib"
+    NO_CMAKE_SYSTEM_PATH
 )
 
-# Determine installation base (prefer GUROBI_DIR, otherwise GUROBI_HOME)
-if(NOT GUROBI_DIR)
-    set(_GurobiBase "$ENV{GUROBI_HOME}")
-else()
-    set(_GurobiBase "${GUROBI_DIR}")
-endif()
 
-# Find all versioned C++ API static archives in ${_GurobiBase}/lib
+# Find all versioned C++ API static archives in ${GUROBI_HOME}/lib
 file(GLOB _ALL_GXX_LIBS
     LIST_DIRECTORIES false
-    "${_GurobiBase}/lib/libgurobi_g++*.a"
+    "${GUROBI_HOME}/lib/libgurobi_g++*.a"
 )
 
 # Select the archive with the highest version number
@@ -45,12 +92,12 @@ if(_best_lib)
     message(STATUS "Using Gurobi C++ API: ${GUROBI_CXX_LIBRARY}")
 else()
     #message(WARNING
-    #    "No libgurobi_g++*.a found in ${_GurobiBase}/lib. "
+    #    "No libgurobi_g++*.a found in ${GUROBI_HOME}/lib. "
     #    "Falling back to gurobi_c++.a."
     #)
     find_library(GUROBI_CXX_LIBRARY
         NAMES     gurobi_c++
-        HINTS     ${_GurobiBase} $ENV{GUROBI_HOME}
+        HINTS     ${GUROBI_HOME}
         PATH_SUFFIXES lib
     )
     if(NOT GUROBI_CXX_LIBRARY)
